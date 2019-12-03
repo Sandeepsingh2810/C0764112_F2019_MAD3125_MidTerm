9b1c3a848cee0fd974fb1b52aeb47ba1fe9ba293
macstudent <sandeepjangra01002gmail.com>
2019-12-02 19:32:22 -0500

    modication

--- app/src/main/java/com/example/c0764112_f2019_mad3125_midterm/DisplayDataActivity.java
+++ app/src/main/java/com/example/c0764112_f2019_mad3125_midterm/DisplayDataActivity.java
@@ -20,7 +20,8 @@ public class DisplayDataActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_data);
 
