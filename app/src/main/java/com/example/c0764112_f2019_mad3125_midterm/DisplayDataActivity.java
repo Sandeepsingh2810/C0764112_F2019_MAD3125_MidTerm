1bc055441072c0117dcd9b6c3385fb04822097bb
macstudent <sandeepjangra01002gmail.com>
2019-12-02 19:32:51 -0500

    displaying

--- app/src/main/java/com/example/c0764112_f2019_mad3125_midterm/DisplayDataActivity.java
+++ app/src/main/java/com/example/c0764112_f2019_mad3125_midterm/DisplayDataActivity.java
@@ -22,6 +22,7 @@ public class DisplayDataActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_data);
 
